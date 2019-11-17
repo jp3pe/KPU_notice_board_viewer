a8fe5e7daf47de593a5212be9a8d4862682f0824
jp3pe <rlaworhkd430430@gmail.com>
2019-11-17 11:17:17 +0900

    Test parser function and extract title from post

--- paser.py
+++ paser.py
@@ -1,3 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
+# test GET page from url
+page = requests.get('http://www.kpu.ac.kr/front/boardview.do?pkid=84137&currentPage=1&searchField=ALL&siteGubun=14&menuGubun=2&bbsConfigFK=1&searchLowItem=ALL&searchValue=')
+# Test soupfy 
+soup = BeautifulSoup(page.text, 'html.parser')
+title = soup.find(class_='subject')
+
+detail = soup.find(class_='article')
+print(title.string)
+print(detail.string)
